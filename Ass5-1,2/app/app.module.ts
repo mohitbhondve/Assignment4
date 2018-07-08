12359345d6fca722ff9948056579a75eabf5bf0c
dhanajiwadne <37262830+dhanajiwadne@users.noreply.github.com>
2018-07-09 01:40:40 +0530

    First Commit

---
+++ Ass5-1,2/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+    ],
+
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+
+  providers: [],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
